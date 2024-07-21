22b7234ee28fe64a70a70f5b40d15f8053076024
AlzateJJ <124931160+AlzateJJ@users.noreply.github.com>
2024-07-20 23:11:24 -0500

    added lazy

--- src/assets/components/LoadingCharacterCard.jsx
+++ src/assets/components/LoadingCharacterCard.jsx
@@ -7,7 +7,7 @@ const LoadingCharacterCard = () => {
     return (
         <article className='character__card'>
             <header className='character__img-container'>
-                <img className='character__img' src='../../../public/loading_img.jpg' alt="Imágen de carga" />
+                <img className='character__img' src='../../../loading_img.jpg' alt="Imágen de carga" />
             </header>
             <section className='character__info-section'>
                 <h3 className='character__info character__name'>Cargando personaje...</h3>
